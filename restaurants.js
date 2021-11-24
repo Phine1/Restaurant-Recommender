16631d7e1085ec670fcf8c53abd9d124a79f2352
Phinehas Sersah <phinehas.sersah@amalitech.org>
2021-11-24 08:52:00 +0000

    first commit

---
+++ restaurants.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = [
+    {
+        restaurantName: 'Silver Rice Sushi 🍣',
+        priceBracket: '2',
+        deliveryTimeMinutes: 40,
+        openHour: '12',
+        closeHour: '23',
+        distance: '5',
+    },
+    {
+        restaurantName: 'Nikko\'s Rotisserie Chicken 🍗',
+        priceBracket: '1',
+        deliveryTimeMinutes: 20,
+        openHour: '12',
+        closeHour: '21',
+        distance: '8',
+    },
+    {
+        restaurantName: 'Aita Trattoria 🍝',
+        priceBracket: '3',
+        deliveryTimeMinutes: 60,
+        openHour: '18',
+        closeHour: '22',
+        distance: '1',
+    },
+    {
+        restaurantName: 'Lula Bagel 🥯',
+        priceBracket: '1',
+        deliveryTimeMinutes: 20,
+        openHour: '0',
+        closeHour: '12',
+        distance: '2',
+    },
+    {
+        restaurantName: 'Golden Chopstick 🥢',
+        priceBracket: '1',
+        deliveryTimeMinutes: 20,
+        openHour: '15',
+        closeHour: '23',
+        distance: '12',
+    },
+];
---
+++ restaurants.ts
@@ -0,0 +1,43 @@
+export default [
+    {
+      restaurantName: 'Silver Rice Sushi 🍣',
+      priceBracket: '2',
+      deliveryTimeMinutes: 40,
+      openHour: '12',
+      closeHour: '23',
+      distance: '5',
+    },
+    {
+      restaurantName: 'Nikko\'s Rotisserie Chicken 🍗',
+      priceBracket: '1',
+      deliveryTimeMinutes: 20,
+      openHour: '12',
+      closeHour: '21',
+      distance: '8',
+    },
+    {
+      restaurantName: 'Aita Trattoria 🍝',
+      priceBracket: '3',
+      deliveryTimeMinutes: 60,
+      openHour: '18',
+      closeHour: '22',
+      distance: '1',
+    },
+    {
+      restaurantName: 'Lula Bagel 🥯',
+      priceBracket: '1',
+      deliveryTimeMinutes: 20,
+      openHour: '0',
+      closeHour: '12',
+      distance: '2',
+    },
+    {
+      restaurantName: 'Golden Chopstick 🥢',
+      priceBracket: '1',
+      deliveryTimeMinutes: 20,
+      openHour: '15',
+      closeHour: '23',
+      distance: '12',
+    },
+  ];
+  
